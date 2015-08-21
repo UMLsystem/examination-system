df1551a518886fec722a6928ead3946d7ca510c9
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-21 18:29:35 +0800

    delete the connection js file

--- db-connection/connection.js
+++
@@ -1,8 +0,0 @@
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('exam_system', 'exam', 'exam', {
-    host: 'localhost',
-    port: '3306',
-    dialect: 'mysql'
-});
-
-module.exports = sequelize;
