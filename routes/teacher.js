2e43a3c477c3ce5edd1e7d6964e12ecd24ee1f77
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-21 16:31:04 +0800

    test find the exam

--- routes/teacher.js
+++ routes/teacher.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var Exam = models.exam;
+
+Exam.findAll().then(function(data) {
+    console.log(data[1].dataValues);
+});
 
 router.get('/', function(req, res, next) {
   res.render('teacher');
