cf60a0d18ae3b3a0f002ed9e01489108b9c14ad6
ldyyukito <ldyyukito@outlook.com>
2015-08-22 11:12:54 +0800

    add a webpage

--- models/question.js
+++ models/question.js
@@ -15,4 +15,4 @@ module.exports = function(sequelize, DataTypes) {
     timestamps: false
   });
   return Question;
-}
+};
--- models/question_paper.js
+++ models/question_paper.js
@@ -12,4 +12,4 @@ module.exports = function(sequelize, DataTypes) {
     timestamps: false
   });
   return QuestionPaper;
-}
+};
