d5c12172a374cf37d29189d75b27e70a41cfdbfe
AnliHuer <AnliHuer@outlook.com>
2015-08-22 20:28:44 +0800

    changed the webpage of usr welcome

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var login = require('./routes/login');
 var users = require('./routes/users');
 var teacher = require('./routes/teacher');
 
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
-app.use('/', routes);
+app.use('/user', login);
 app.use('/users', users);
 app.use('/teacher', teacher);
 // catch 404 and forward to error handler
--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,22 +1,37 @@
+$().ready(function() {
+  $("#form-signin").validate({
+    rules: {
+      inputUsername: "required",
+      inputPassword: {
+        required: true,
+        minlength: 6,
+        maxlength: 12
+      },
+    },
+    messages: {
+      inputUsername: "请输入姓名",
+      inputPassword: {
+        required: "请输入密码",
+        minlength: jQuery.format("密码不能小于6个字符"),
+        maxlength: jQuery.format("密码不能大于12个字符")
+      },
+    }
+  });
+});
+
 $(function() {
-  $('button').on('click', function() {
+  $('#submit').on('click', function() {
     var username = $('#inputUsername').val();
     var password = $('#inputPassword').val();
-    if (username.length === 0) {
-      $('.label-username').html('用户名不能为空!');
-    } else if (password.length === 0) {
-      $('.label-password').html('密码不能为空!');
-    } else {
-      $.get('/login', {
-        username: username,
-        password: password
-      }, function(resq) {
-        if (resq.stu_num === username && resq.stu_pwd === password) {
-
-        } else {
-          $('.label-username').html('用户名或密码错误,请重新输入!');
-        }
-      });
-    }
+    $.get('/user', {
+      username: username,
+      password: password
+    }, function(resq) {
+      if (resq.stu_num === username && resq.stu_pwd === password) {
+        $.session.set('stuId', stu_id);
+      } else {
+        $('.label-username').html('用户名或密码错误,请重新输入!');
+      }
+    });
   });
 });
