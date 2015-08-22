a25ade35603db4e98e78ad7264055c0c7845f472
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-22 09:53:42 +0800

    set the rount

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var teacher = require('./routes/teacher');
-var paperList = require('./routes/paperList');
+var route = require('./routes/route');
 
 var app = express();
 
@@ -23,12 +19,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/teacher', teacher);
-app.use('/paperList', paperList);
-app.use('/score',score);
+route.setRoutes(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
