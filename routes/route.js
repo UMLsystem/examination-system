eee0b02492c98493b91dc2aa7f0f54922a2fd8d0
sissie <sissiescofield@outlook.com>
2015-08-22 10:36:07 +0800

    delete the files outside "routers"

--- routes/route.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.setRoutes = function(app) {
-    app.use('/', require('./routers/index'));
-    app.use('/users',require('./routers/users'));
-    app.use('/teacher',require('./routers/teacher'));
-    app.use('/paperList',require('./routers/paperList'));
-
-};
