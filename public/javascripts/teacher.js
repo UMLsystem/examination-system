cdc741e7163baac0bde0d9cb1f602a03a521fb0b
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-22 18:43:18 +0800

    add class check in the td click

--- public/javascripts/teacher.js
+++ public/javascripts/teacher.js
@@ -1,11 +1,14 @@
 $(function() {
-    $('.check').on('click', function() {
+    $('tbody td').on('click', function() {
         var status = this.innerHTML.split('<')[0];
-
-        if (status === 'false') {
-            alert('未开放！')
-        } else {
-            $(location).attr('href', '/');
-        }
+        var temp = status;
+        
+        $('.check').on('click', function() {
+            if (temp === 'false') {
+                alert('未开放！')
+            } else {
+                $(location).attr('href', '/');
+            }
+        });
     });
 });
