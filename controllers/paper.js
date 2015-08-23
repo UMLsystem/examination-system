7196b1b36d1bf5d5b6180ad903247a118140f58f
sherrypan <sherrypann@outlook.com>
2015-08-23 14:07:13 +0800

    modify controllers/paper.js

--- controllers/paper.js
+++ controllers/paper.js
@@ -3,35 +3,77 @@ var Paper = models.paper;
 var QuestionPaper = models.question_paper;
 var Question = models.question;
 var Type = models.type;
+var contents = [];
+
 
 function PaperController() {
 
 }
+
+
 PaperController.prototype.getContent = function(req, res) {
-  var ques = [];
-  //var exa_id = req.exa_id;
   var exa_id = 1;
   Paper.findById(exa_id).then(function(data) {
-    paper_id = data.dataValues.pap_id;
-    console.log(paper_id);
-    QuestionPaper.findAll({
-      where: {
-        pap_id: paper_id
+    return getPaperId(exa_id, data);
+  }).then(function(data) {
+    return getQuestionIds(data);
+  }).then(function(data) {
+    return getQuestionContents(data);
+  }).then(function(data) {
+    var paperContent = getPaperContent(contents)
+      //  res.send(paperContent);
+    res.send(paperContent);
+  })
+}
+
+function getPaperId(exa_id, data) {
+  var paper_id = data.dataValues.pap_id;
+  return QuestionPaper.findAll({
+    where: {
+      pap_id: paper_id
+    }
+  })
+}
+
+function getQuestionIds(data) {
+  var question_ids = data.map(function(val) {
+    return val.dataValues.que_id;
+  })
+  return Question.findAll({
+    where: {
+      que_id: {
+        $in: question_ids
       }
-    }).then(function(questions) {
-      questions.forEach(function(val) {
-        Question.findById(val.dataValues.que_id).then(function(que) {
-          console.log(que.dataValues);
-          ques.push(que.dataValues);
-          // Type.findById(que.dataValues.type_id).then(function(type) {
-          //   console.log(type.dataValues);
-          // });
-        });
-      })
-    }).done(function() {
-      res.render('paper')
-    });
+    }
+  })
+}
+
+function getQuestionContents(data) {
+  contents = data.map(function(val) {
+    return {
+      content: val.dataValues.que_content,
+      type_id: val.dataValues.typ_id
+    };
+  })
+
+}
+
+function getPaperContent(contents) {
+  var paperContent = {
+    'blank': [],
+    'single': [],
+    'mult': []
+  };
+  contents.forEach(function(val) {
+    if (val.type_id === 1) {
+      paperContent['blank'].push(val.content);
+    } else if (val.type_name === 2) {
+      paperContent[singleSelect].push(val.content);
+    } else if (val.type_name === 3) {
+      paperContent[mulSelect].push(val.content);
+    }
   });
+  return paperContent;
 }
 
 module.exports = PaperController;
---
+++ controllers/teacher.js
@@ -0,0 +1,21 @@
+var models = require('../models');
+var Exam = models.exam;
+
+function TeacherController() {
+
+}
+TeacherController.prototype.getList = function(req, res, next) {
+    var array = [];
+
+    Exam.findAll().then(function(data) {
+        data.forEach(function(val) {
+            array.push(val.dataValues);
+        })
+    }).done(function() {
+        res.render('teacher', {
+            array: array
+        });
+    });
+}
+
+module.exports = TeacherController;
