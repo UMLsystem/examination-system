0e39c027f7f0d0388e67495379c98c8f6ce25842
sissie <sissiescofield@outlook.com>
2015-08-25 12:22:51 +0800

    delete the old tables

--- models/teacher.js
+++ models/teacher.js
@@ -1,13 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
   var Teacher = sequelize.define('Teacher', {
-    id: {
+    tea_id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true
     },
-    teacherName: DataTypes.STRING,
-    teacherNumber: DataTypes.INTEGER,
-    teacherPassword: DataTypes.STRING
+    tea_name: DataTypes.STRING,
+    tea_num: DataTypes.INTEGER,
+    tea_pwd: DataTypes.STRING
   }, {
     freezeTableName: true,
     timestamps: false
