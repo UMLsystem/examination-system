9d64809f9ef53247ffee493979afb71c59415031
laetella <laetella@outlook.com>
2015-08-25 10:22:15 +0800

    use once query

--- controllers/paperList.js
+++ controllers/paperList.js
@@ -4,94 +4,39 @@ var Exam = models.exam;
 
 
 function PaperListQuery() {
-  // var exams = [];
-}
-
-// PaperListQuery.prototype.getExamedList = function(req, res, next) {
-//   var exams = [];
-//   var examList = {};
-//   StudentExam.findAll({
-//     where : {
-//       studentId : req.studentId
-//     }
-//   }).then(function(data) {
-//     var examIds = data.map(function (val) {
-//       examList.examId = val.examId;
-//       examList.status = val.status;
-//       return val.examId;
-//     });
-//     Exam.findAll({
-//       where : {
-//         id : {
-//           $in : examIds
-//         }
-//       }
-//     }).then(function(data) {
-//       var examList = data.map(function(val) {
-//         examList.examName = val.examName;
-//         return examList;
-//       })
-//       res.render('paperList',{examList:examList});
-//     })
-//   });
-// }
-
-PaperListQuery.prototype.getExamedList = function(req, res, next) {
   var exams = {
     examed: [],
     toexam: []
   };
-  var singleExamed = {};
+}
+
+PaperListQuery.prototype.getExamedList = function(req, res, next) {
+  var exams = [];
+  var examList = {};
   StudentExam.findAll({
-    where: {
-      stu_id: 1,
-      status: true
+    where : {
+      studentId : req.studentId
     }
   }).then(function(data) {
-    var examIds = data.map(function(val) {
-      return val.exa_id;
+    var examIds = data.map(function (val) {
+      examList.examId = val.examId;
+      examList.status = val.status;
+      return val.examId;
     });
     Exam.findAll({
-      where: {
-        exa_id: {
-          $in: examIds
+      where : {
+        id : {
+          $in : examIds
         }
       }
     }).then(function(data) {
-      data.map(function(val) {
-        singleExamed.exa_name = val.exa_name;
-        singleExamed.exa_id = val.exa_id;
-        return singleExamed;
+      var examList = data.map(function(val) {
+        examList.examName = val.examName;
+        return examList;
       })
-      exams.examed.push(singleExamed);
+      res.render('paperList',{examList:examList});
     })
-    StudentExam.findAll({
-      where: {
-        stu_id: 1,
-        status: false
-      }
-    }).then(function(data) {
-      var examIds = data.map(function(val) {
-        return val.exa_id;
-      });
-      Exam.findAll({
-        where: {
-          exa_id: {
-            $in: examIds
-          }
-        }
-      }).then(function(data) {
-        data.map(function(val) {
-          singleExamed.exa_name = val.exa_name;
-          singleExamed.exa_id = val.exa_id;
-          return singleExamed;
-        })
-        exams.toexam.push(singleExamed);
-        console.log(exams);
-        return exams;
-        res.render('/paperList',{exams:exams});
-      })
-    });
   });
 }
+
 module.exports = PaperListQuery;
