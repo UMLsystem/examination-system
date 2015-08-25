2be742db592683347e10c3e88c5d973ddb717947
laetella <laetella@outlook.com>
2015-08-25 10:08:17 +0800

    modify router paperList.js

--- routes/routers/paperList.js
+++ routes/routers/paperList.js
@@ -4,10 +4,6 @@ var router = express.Router();
 var PaperListQuery = require('../../controllers/paperList');
 var query = new PaperListQuery();
 
-router.get('/', function(req, res) {
-  console.log(req.query.stuId + '7777');
-  query.getList(req.query.stuId);
-  res.render('paperList');
-});
+router.get('/', query.getExamedList);
 
 module.exports = router;
