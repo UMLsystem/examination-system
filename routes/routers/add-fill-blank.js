5ddaf3c79858546ae4d59bf7db939367f334b10a
Danaekola <danaekola@outlook.com>
2015-08-26 18:20:27 +0800

    modify filename

--- routes/routers/add-fill-blank.js
+++ routes/routers/add-fill-blank.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router = express.Router();
+<<<<<<< bf7d31d9a9c6cddfb4209451b1dbaef70569fa8d
 var QuestionInsertion = require('../../controllers/question-insert');
 var questionInsert = new QuestionInsertion();
+=======
+
+var QuestionInsertion = require('../../controllers/question-insert');
+var questionInsert = new QuestionInsertion();
+
+>>>>>>> modify filename
 router.post('/',questionInsert.insertFill);
 
 module.exports = router;
