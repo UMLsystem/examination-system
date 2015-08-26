3f3bf8cf688cd4c0bd3891051d675528243cb950
sherrypan <sherrypann@outlook.com>
2015-08-26 18:20:59 +0800

    modify controllers/paper.js

--- controllers/paper.js
+++ controllers/paper.js
@@ -11,30 +11,6 @@ function PaperController() {}
 
 getAllTypes();
 
-PaperController.prototype.show = function(req, res) {
-  var examId = req.query.examId;
-  Paper.findById(examId).then(function(data) {
-    return QuestionPaper.findAll({
-      where: {
-        paperId: data.dataValues.id
-      },
-      include: [{
-        model: Question,
-        where: {
-          id: Sequelize.col('QuestionPaper.questionId')
-        }
-      }]
-    });
-  }).then(function(data) {
-    var paperContent = getQuestionContents(data);
-    res.render('paper', {
-      blank: paperContent.blank,
-      single: paperContent.single,
-      multiple: paperContent.multiple
-    });
-  });
-};
-
 function getAllTypes() {
   Type.getAllTypes().then(function(data) {
     types = data.map(function(val) {
@@ -68,6 +44,31 @@ function processData(val, paperContent) {
         'D': result[4]
       });
     }
+  });
+}
+
+PaperController.prototype.show = function(req, res) {
+  var examId = req.query.examId;
+  var examId = 1;
+  Paper.findById(examId).then(function(data) {
+    return QuestionPaper.findAll({
+      where: {
+        paperId: data.dataValues.id
+      },
+      include: [{
+        model: Question,
+        where: {
+          id: Sequelize.col('QuestionPaper.questionId')
+        }
+      }]
+    });
+  }).then(function(data) {
+    var paperContent = getQuestionContents(data);
+    res.render('paper', {
+      blanks: paperContent.blank,
+      singleChoices: paperContent.single,
+      multipleChoices: paperContent.multiple
+    });
   });
 }
 
