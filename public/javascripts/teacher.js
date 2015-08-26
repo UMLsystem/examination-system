061513334bb10f1dc861fcdca778b843fe0eb05f
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-26 14:55:08 +0800

    beautify the code from

--- public/javascripts/teacher.js
+++ public/javascripts/teacher.js
@@ -1,12 +1,12 @@
 $(function() {
     $('table tbody tr').each(function() {
-           var row = $("td", this);
-           var status = row.eq(2).text().trim();
-           if(status === "true") {
-               row.eq(2).text('已考');
-           }else {
-               row.eq(2).text('未考');
-           }
+        var row = $("td", this);
+        var status = row.eq(2).text().trim();
+        if (status === "true") {
+            row.eq(2).text('已考');
+        } else {
+            row.eq(2).text('未考');
+        }
     });
 
     $('.check').on('click', function() {
