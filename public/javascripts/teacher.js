b6a71b3ee0b8aa7cf6ddedf85b4525856ce1210d
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-26 11:00:21 +0800

    add a td called operation

--- public/javascripts/teacher.js
+++ public/javascripts/teacher.js
@@ -1,11 +1,19 @@
 $(function() {
-    $('.check').on('click', function() {
+    $('table tbody tr').each(function() {
+           var row = $("td", this);
+           var status = row.eq(2).text().trim();
+           if(status === "true") {
+               row.eq(2).text('已考');
+           }else {
+               row.eq(2).text('未考');
+           }
+    });
 
+    $('.check').on('click', function() {
         var status = $(this).data('status');
 
         if (status === false) {
-            alert('未开放！');
-          //  $(this).closest('td').html()='未考';
+            alert('还未进行该门课程考试');
         } else {
             $(location).attr('href', '/');
         }
