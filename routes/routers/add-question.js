98717e623db241ad92f1f3988427e747069ebdfe
Danaekola <danaekola@outlook.com>
2015-08-26 16:52:43 +0800

    delete the layout option in add-question

--- routes/routers/add-question.js
+++ routes/routers/add-question.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  res.render('add-question',   { layout: 'default' });
+  res.render('add-question');
 });
 
 module.exports = router;
