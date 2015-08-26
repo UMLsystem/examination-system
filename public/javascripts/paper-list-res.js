4ea7854d77df9df1adc04dd32ff069125208c531
laetella <laetella@outlook.com>
2015-08-26 15:13:54 +0800

    add alert of response

--- public/javascripts/paper-list-res.js
+++ public/javascripts/paper-list-res.js
@@ -1,15 +1,24 @@
 var stuId = $.cookie('stuId');
+
 $(function() {
   $.get('/paperList', {
     stuId: stuId
-  }, function(resp) {});
+  }, function(resp) {
+
+  });
+
   $('.check').on('click', function() {
     var status = $(this).data('status');
-    var examId = $(this).parent().parent().data('id');
-    $.get('/paper', {
-      examId: examId
-    }, function() {
+    if (false === status) {
+      var examId = $(this).parent().parent().data('id');
+      $.get('/paper', {
+        examId: examId
+      }, function(resp) {
 
-    });
+      });
+    } else {
+      alert('You have attended this exam.');
+    }
   });
+
 });
