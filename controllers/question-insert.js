aa0f630c39f78287746d0e0c9ab33af53bd4eeee
Danaekola <danaekola@outlook.com>
2015-08-26 12:04:08 +0800

    hah

--- controllers/question-insert.js
+++ controllers/question-insert.js
@@ -1,41 +1,107 @@
 var models = require('../models');
 var Question = models.Questions;
-var Type = models.Types;
+var Type = models.Type;
+var Option = models.Options;
 
 function QuestionInsert(){
 
 }
 
-QuestionInsert.prototype.insertFill = function(req,res){
+QuestionInsertion.prototype.insertFill = function(req, res) {
+  res.cookie('tecaherId',8);
+  var teacherId = req.cookies.teacherId;
+  var form = req.body;
+  var FILL_ID = 1;
+  var TYPE_ID = FILL_ID;
+  var question = Question.create({
+    typeId: TYPE_ID,
+    teacherId: teacherId,
+    question: form.question,
+    answer: form.answer
+  }).then(function() {
+    if (question) {
+      res.render('add-success');
+    } else {
+      res.render('failture');
+    }
+  });
+};
 
-   var form = req.body;
-
-
-
-   var que = Question.create({typeId: 1,teacherId:1,question:form.question,answer:form.answer}).then(function(){
-     res.render('add-success');
-   });
+QuestionInsertion.prototype.insertSingle = function(req, res) {
+    res.cookie('tecaherId',8);
+  var teacherId = req.cookies.teacherId;
+  var form = req.body;
+  var SINGLE_ID = 2;
+  var TYPE_ID = SINGLE_ID;
+  var question = Question.build({
+    typeId: TYPE_ID,
+    teacherId: teacherId,
+    question: form.question,
+    answer: form.singleChoices
+  });
+  question.save().then(function() {
+    console.log("question="+JSON.stringify(question));
+    console.log("option="+form.optionOne);
+    console.log("questionId="+question.id);
+    Option.bulkCreate([{
+      option: form.optionOne,
+      questionId: question.id
+    }, {
+      option: form.optionTwo,
+      questionId: question.id
+    }, {
+      option: form.optionThree,
+      questionId: question.id
+    }, {
+      option: form.optionFour,
+      questionId: question.id
+    }]);
+  }).then(function() {
+    if (question) {
+      res.render('add-success');
+    } else {
+      res.render('failture');
+    }
+  });
 
 };
-QuestionInsert.prototype.insertSingle = function(req,res){
 
-   var form = req.body;
-
-
-
-   var que = Question.create({typeId: 2,teacherId:1,question:form.question+'#'+form.optionOne+'#'+form.optionTwo+'#'+form.optionThree+'#'+form.optionFour,answer:form.singleChoices}).then(function(){
-       res.render('add-success');
-   });
-
-};
-QuestionInsert.prototype.insertMultiple = function(req,res){
-
-   var form = req.body;
-
-
-   var que = Question.create({typeId: 3,teacherId:1,question:form.question+'#'+form.optionOne+'#'+form.optionTwo+'#'+form.optionThree+'#'+form.optionFour,answer:form.multipleChoices[0]+form.multipleChoices[1]+form.multipleChoices[2]}).then(function(){
-       res.render('add-success');
-   });
-
+QuestionInsertion.prototype.insertMultiple = function(req, res) {
+    res.cookie('tecaherId',8);
+  var teacherId = req.cookies.teacherId;
+  var form = req.body;
+  var string = '';
+  form.multipleChoices.forEach(function(multiple) {
+    string += multiple;
+  });
+  var MULTIPLE_ID = 3;
+  var TYPE_ID = MULTIPLE_ID;
+  var question = Question.create({
+    typeId: TYPE_ID,
+    teacherId: teacherId,
+    question: form.question,
+    answer: string
+  });
+  question.save().then(function() {
+    return Option.bulkCreate([{
+      option: form.optionOne,
+      questionId: question.id
+    }, {
+      option: form.optionTwo,
+      questionId: question.id
+    }, {
+      option: form.optionThree,
+      questionId: question.id
+    }, {
+      option: form.optionFour,
+      questionId: question.id
+    }]).then(function() {
+      if (question) {
+        res.render('add-success');
+      } else {
+        res.render('failture');
+      }
+    });
+  });
 };
 module.exports = QuestionInsert;
