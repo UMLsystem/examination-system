4ee071b649b4b68f7e5c64f1db4db9852e5e615e
ldyyukito <ldyyukito@outlook.com>
2015-08-26 22:52:16 +0800

    delete explaintion

---
+++ controllers/score-controller.js
@@ -0,0 +1,14 @@
+var models = require('../models');
+var Sequelize = require('sequelize');
+var Type = models.Type;
+
+function ScoreController() {
+
+}
+ScoreController.prototype.markExam = function(req, res, next) {
+  res.send(req.body);
+};
+
+
+
+module.exports = ScoreController;
