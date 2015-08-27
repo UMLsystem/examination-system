d0db85be297f69015a6cdc6e7a18ca26f09029f0
sherrypan <sherrypann@outlook.com>
2015-08-27 15:39:50 +0800

    change value of name from id to text_id

--- view-helpers/paper-helper.js
+++ view-helpers/paper-helper.js
@@ -4,8 +4,7 @@ hbs.registerHelper('blank', function(options) {
   var text = '';
   for (var i = 0; i < options.length; i++) {
     var questionNo = i + 1;
-    text += "<h4>" + questionNo + ' . ' + options[i].content +  "</h4>\n"
-          + "<input type='text' name=" + options[i].id + " value=''></br>";
+    text += "<h4>" + questionNo + ' . ' + options[i].content + "</h4>\n" + "<input type='text' name="+'text_'+options[i].id + " ></br>";
   }
   return new hbs.SafeString(text);
 });
@@ -14,10 +13,11 @@ hbs.registerHelper('singleChoice', function(options) {
   var text = '';
   for (var i = 0; i < options.length; i++) {
     var questionNo = i + 1;
+    console.log(options[i].id);
     text += "<h4>" + questionNo + ' . ' + options[i].content + "</h4>\n";
-    for (var j = 0; j < options[i].options; j++) {
+    for (var j = 0; j < options[i].options.length; j++) {
       var choiceNumber = String.fromCharCode(constant.ASCII_A + j);
-      text += "<input type='radio' name=" + options[i].options[j] + "value=" + choiceNumber + ">" + choiceNumber + '\n';
+      text += "<h4><input type='radio' name=" + 'radio_' + options[i].id + " value=" + choiceNumber + ">" + choiceNumber + '.' + options[i].options[j] + '</h4>';
     }
   }
   return new hbs.SafeString(text);
@@ -27,10 +27,10 @@ hbs.registerHelper('multipleChoice', function(options) {
   var text = '';
   for (var i = 0; i < options.length; i++) {
     var questionNo = i + 1;
-    text += "<h4>" + questionNo + ' . ' + options[i].content +  "</h4>\n";
-    for(var j = 0; j < options[i].options.length;j++){
-      var choiceNumber = String.fromCharCode(constant.ASCII_A+j);
-      text += "<h4><input type='checkbox' name="+options[i].options[j]+"value="+choiceNumber+">"+choiceNumber+'.'+options[i].options[j]+'</h4>';
+    text += "<h4>" + questionNo + ' . ' + options[i].content + "</h4>\n";
+    for (var j = 0; j < options[i].options.length; j++) {
+      var choiceNumber = String.fromCharCode(constant.ASCII_A + j);
+      text += "<h4><input type='checkbox' name=" +'checkbox_'+ options[i].id + " value=" + choiceNumber + ">" + choiceNumber + '.' + options[i].options[j] + '</h4>';
     }
   }
   return new hbs.SafeString(text);
