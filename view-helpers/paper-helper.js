92912984da0a1d7bed8bdc08da784b951f563c6e
sherrypan <sherrypann@outlook.com>
2015-08-27 15:01:15 +0800

    changed constant.A to ASCII_A

--- view-helpers/paper-helper.js
+++ view-helpers/paper-helper.js
@@ -14,10 +14,10 @@ hbs.registerHelper('singleChoice', function(options) {
   var text = '';
   for (var i = 0; i < options.length; i++) {
     var questionNo = i + 1;
-    text += "<h4>" + questionNo + ' . ' + options[i].content +  "</h4>\n";
-    for(var j = 0; j < options[i].options;j++){
-      var choiceNumber = String.fromCharCode(constant.A+j);
-      text += "<input type='radio' name="+options[i].options[j]+"value="+choiceNumber+">"+choiceNumber+'\n';
+    text += "<h4>" + questionNo + ' . ' + options[i].content + "</h4>\n";
+    for (var j = 0; j < options[i].options; j++) {
+      var choiceNumber = String.fromCharCode(constant.ASCII_A + j);
+      text += "<input type='radio' name=" + options[i].options[j] + "value=" + choiceNumber + ">" + choiceNumber + '\n';
     }
   }
   return new hbs.SafeString(text);
@@ -28,9 +28,9 @@ hbs.registerHelper('multipleChoice', function(options) {
   for (var i = 0; i < options.length; i++) {
     var questionNo = i + 1;
     text += "<h4>" + questionNo + ' . ' + options[i].content +  "</h4>\n";
-    for(var j = 0; j < options[i].options;j++){
-      var choiceNumber = String.fromCharCode(constant.A+j);
-      text += "<input type='checkbox' name="+options[i].options[j]+"value="+choiceNumber+">"+choiceNumber+'\n';
+    for(var j = 0; j < options[i].options.length;j++){
+      var choiceNumber = String.fromCharCode(constant.ASCII_A+j);
+      text += "<h4><input type='checkbox' name="+options[i].options[j]+"value="+choiceNumber+">"+choiceNumber+'.'+options[i].options[j]+'</h4>';
     }
   }
   return new hbs.SafeString(text);
