494a9fe0ae88de7a254d47c62ac8296067ddb653
sherrypan <sherrypann@outlook.com>
2015-08-27 12:48:53 +0800

    add joint query

--- controllers/paper.js
+++ controllers/paper.js
@@ -3,8 +3,10 @@ var Sequelize = require('sequelize');
 var Paper = models.Paper;
 var QuestionPaper = models.QuestionPaper;
 var Question = models.Question;
+var Option = models.Option;
 var Type = models.Type;
 var contents = [];
+var questions = [];
 var types = [];
 
 function PaperController() {}
@@ -21,53 +23,83 @@ function getAllTypes() {
 
 function getQuestionContents(data) {
   var paperContent = {};
-  data.map(function(val) {
-    return val.dataValues.Questions[0].dataValues;
-  }).forEach(function(val) {
+  data.forEach(function(val) {
     processData(val, paperContent);
   });
   return paperContent;
 }
 
+
 function processData(val, paperContent) {
   types.forEach(function(type) {
     if (type.id === val.typeId) {
       var key = type.typeName;
-      var result = val.question.split('#');
       paperContent[key] = paperContent[key] || [];
       paperContent[key].push({
-        content: result[0],
-        'A': result[1],
-        'B': result[2],
-        'C': result[3],
-        'D': result[4]
+        content: val.question,
+        options: val.options,
+        id: val.id
       });
     }
   });
 }
 
 PaperController.prototype.show = function(req, res) {
-  var examId = 1; //var examId = req.query.examId;
-  Paper.findById(examId).then(function(data) {
-    return QuestionPaper.findAll({
+  var examId = req.query.examId;
+  QuestionPaper.findAll({
+    include: [{
+      model: Paper,
       where: {
-        paperId: data.dataValues.id
-      },
-      include: [{
-        model: Question,
-        where: {
-          id: Sequelize.col('QuestionPaper.questionId')
-        }
-      }]
+        examId: examId,
+        id: Sequelize.col('QuestionPaper.paperId')
+      }
+    }],
+    include: [{
+      model: Question,
+      where: {
+        id: Sequelize.col('QuestionPaper.questionId')
+      }
+    }]
+  }).then(function(data) {
+
+    questions = data.map(function(val) {
+      return val.dataValues.Questions[0].dataValues;
+    });
+    return questions;
+  }).then(function(data) {
+    var ids = data.map(function(val) {
+      return val.id;
+    });
+    return Option.findAll({
+      questionId: {
+        $in: ids
+      }
     });
   }).then(function(data) {
-    var paperContent = getQuestionContents(data);
+    var options = data.map(function(val) {
+      return val.dataValues;
+    });
+    getContent(questions, options);
+    var paperContent = getQuestionContents(contents);
     res.render('paper', {
       blanks: paperContent.blank,
-      singleChoices: paperContent.single,
-      multipleChoices: paperContent.multiple
+      singleChoices: paperContent.singleChoices,
+      multipleChoices: paperContent.multipleChoices
     });
   });
 }
 
+function getContent(questions, options) {
+  options.forEach(function(option) {
+
+    contents = questions.map(function(question) {
+      if (option.questionId === question.id) {
+        question['options'] = question['options'] || [];
+        question['options'].push(option.option);
+      }
+      return question;
+    });
+  });
+  return contents;
+}
 module.exports = PaperController;
