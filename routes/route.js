840bedc60c03757bbdfb6cc35e7c5ff76f60286c
laetella <laetella@outlook.com>
2015-08-27 10:31:20 +0800

    correct the error of cannot find module paper-list

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ exports.setRoutes = function(app) {
   app.use('/', require('./routers/index'));
   app.use('/users', require('./routers/users'));
   app.use('/teacher', require('./routers/teacher'));
-  // app.use('/paper-list', require('./routers/paper-list'));
+  app.use('/paper-list', require('./routers/paper-list'));
   app.use('/paper', require('./routers/paper'));
   app.use('/user-validate', require('./routers/user-validate'));
   app.use('/insert-fill', require('./routers/add-fill-blank'));
@@ -13,5 +13,5 @@ exports.setRoutes = function(app) {
   app.use('/add-question', require('./routers/add-question'));
   app.use('/score', require('./routers/mark-exam'));
   app.use('/paper-check', require('./routers/paper-check'));
-  
+
 };
