5b45085c77a52c627435761cc9628561ac8c2736
ldyyukito <ldyyukito@outlook.com>
2015-08-27 14:50:44 +0800

    delete the file because it is same with file called score

---
+++ controllers/multiple-choice.js
@@ -0,0 +1,21 @@
+function MultipleChoice(type, questionId, value, trueValue, score) {
+  this.type = type;
+  this.questionId = questionId;
+  this.value = value;
+  this.trueValue = trueValue;
+  this.score = score;
+}
+
+MultipleChoice.prototype.mark = function() {
+  var finalScore;
+  if (this.value === this.trueValue) {
+    finalScore = this.score;
+  } else if (this.trueValue.match(this.value)) {
+    finalScore = parseInt(this.score / 2);
+  } else {
+    finalScore = 0;
+  }
+  return finalScore;
+};
+
+module.exports = MultipleChoice;
