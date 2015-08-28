7e3d009828df71b92db8177ba600862aa7ff59f6
laetella <laetella@outlook.com>
2015-08-28 19:54:39 +0800

    render page on the back stage

--- controllers/paper-list.js
+++ controllers/paper-list.js
@@ -3,7 +3,6 @@ var User = models.User;
 var Exam = models.Exam;
 var Sequelize = require('sequelize');
 
-
 function PaperListQuery() {
 
 }
@@ -14,7 +13,16 @@ PaperListQuery.prototype.getExamedList = function(req, res, next) {
       userId: 1
     }
   }).then(function(data) {
-    var examList = data.map(function(val) {
+    var examList = data.map(function(val, index) {
+      val.dataValues.examId = val.dataValues.id;
+      val.dataValues.id = index + 1;
+      if(val.dataValues.status === true) {
+        val.dataValues.status = '已考';
+        val.dataValues.button = '查看试卷';
+      } else {
+        val.dataValues.status = '未考';
+        val.dataValues.button = '开始考试';
+      }
       return val.dataValues;
     });
     res.render('paper-list', {
--- public/javascripts/paper-list-res.js
+++ public/javascripts/paper-list-res.js
@@ -1,28 +1,13 @@
 var stuId = $.cookie('stuId');
 
 $(function() {
-  $.get('/paper-list', {
-    stuId: stuId
-  }, function(resp) {});
-
-  var examStatus;
   $('table tbody tr').each(function(i) {
-    $("tbody tr td:first-child")[i].innerHTML = i + 1;
-    var row = $('td', this);
-    var examStatus = $(document.getElementsByName("status")[i]).data("status");
+    $('td', this).on('click', '.check', function() {
+      $.get('/paper', {
+        examId: $(this).data('examId')
+      }, function(resp) {
 
-    if (examStatus === false) {
-      row.eq(2).text('未考');
-      row.eq(3).children().val('开始考试');
-      $(row.eq(3).children()).on('click', function() {
-        $.get('/paper', {
-          examId: examId
-        }, function(resp) {
-
-        });
       });
-    } else {
-      row.eq(2).text('已考');
-    }
+    });
   });
 });
